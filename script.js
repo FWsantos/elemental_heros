7ae9187ae917e4021e9687c8f1ed1d52052b5bdd
FWSantos <fwsantos23@gmail.com>
2019-11-20 07:43:52 -0300

    'update'

--- script.js
+++ script.js
@@ -11,18 +11,53 @@ var enablePlay = function () {
 	btnPlay.removeAttribute('disabled');	
 }
 
+var colorir = function (id) {
+	var obj = document.getElementById('b'+id);
+	obj.classList.toggle(cores[id]);
+	document.getElementById('resultado').innerHTML = id+1;
 
-var play = function () {
+}
+
+var play = function (turnoD) {
 	disablePlay();
 
+	// // var j = 0;
+	// for (var i = cores.length - 1; i >= 0; i--) {
+	// 	(function (i) {
+	// 		setTimeout(function () {
+	// 			console.log("Teste" + i);
+	// 			blocos[i].classList.add(cores[i]);
+	// 			document.getElementById('resultado').innerHTML = i;
+	// 			if(i === 5){
+	// 				// document.getElementById('resultado').innerHTML = ;
+	// 			}
 
-	for (var i = cores.length - 1; i >= 0; i--) {
-		sleep(1000);
-
-		 
-		blocos[i].classList.add(cores[i]);
-		// setInterval(blocos[i].classList.add(cores[i]), 1000)
-
+	// 		}, 100*i);
+	// 	})(i);
+	// }
+	
+	// var turnoD = 3;
+	var a = [];
+	for (var i = 0; i < turnoD; i++) {
+		a[i] = ( Math.floor(Math.random() * 6));
 	}
-	// alert("teste");
+	for (var i = a.length - 1; i >= 0; i--) {
+		(function (i) {
+			setTimeout(function () {
+				if(i != 0){
+					blocos[a[i-1]].classList.remove(cores[a[i-1]]);
+
+				}
+				console.log(a[i]);
+				blocos[a[i]].classList.add(cores[a[i]]);
+				document.getElementById('resultado').innerHTML = a[i] + 1;
+				if(i === 5){
+					// document.getElementById('resultado').innerHTML = ;
+				}
+
+			}, 300*i);
+		})(i);
+	}
+
+	console.log(a);
 }

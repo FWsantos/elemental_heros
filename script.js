421cd2d6c82cb36b114b856e86ff4b2ad3af1e93
aluno <aluno@ufc.br>
2019-11-20 13:52:07 -0300

    Quase

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var cores = ["bg-warning","bg-secondary","bg-primary","bg-info","bg-danger","bg-success"];
 var blocos = document.getElementsByClassName('bloco');
+var permite = false;
 
 var disablePlay = function () {
 	var btnPlay = document.getElementById("play");
@@ -14,50 +15,118 @@ var enablePlay = function () {
 var colorir = function (id) {
 	var obj = document.getElementById('b'+id);
 	obj.classList.toggle(cores[id]);
-	document.getElementById('resultado').innerHTML = id+1;
+	// document.getElementById('resultado').innerHTML = id+1;
+
+}
+
+var payplay = function (a) {
+	// console.log(permite);
+	// window.permite = true;
+	// console.log(permite);
+	
+	var j = 0;
+	
+	var ids = [0, 1, 2, 3, 4, 5]
+	document.getElementById("b0").onclick = function() {
+		colorir(0);
+		if(a[j] == ids[j]){
+			j++;
+		}else{
+			document.getElementById('resultado').innerHTML = "Perdeu";
+			setTimeout(function () {
+				document.location.reload(true);
+			}, 2000)
+		}
+
+	};
+	document.getElementById("b1").onclick = function() {
+		colorir(1);
+		if(a[j] == ids[j]){
+			j++;
+		}else{
+			document.getElementById('resultado').innerHTML = "Perdeu";
+			setTimeout(function () {
+				document.location.reload(true);
+			}, 2000)
+		}
+		
+	};
+	document.getElementById("b2").onclick = function() {
+		colorir(2);
+		if(a[j] == ids[j]){
+			j++;
+		}else{
+			document.getElementById('resultado').innerHTML = "Perdeu";
+			setTimeout(function () {
+				document.location.reload(true);
+			}, 2000)
+		}
+		
+	};
+	document.getElementById("b3").onclick = function() {
+		colorir(3);
+		if(a[j] == ids[j]){
+			j++;
+		}else{
+			document.getElementById('resultado').innerHTML = "Perdeu";
+			setTimeout(function () {
+				document.location.reload(true);
+			}, 2000)
+		}
+		
+	};
+	document.getElementById("b4").onclick = function() {
+		colorir(4);
+		if(a[j] == ids[j]){
+			j++;
+		}else{
+			document.getElementById('resultado').innerHTML = "Perdeu";
+			setTimeout(function () {
+				document.location.reload(true);
+			}, 2000)
+		}
+		
+	};
+	document.getElementById("b5").onclick = function() {
+		colorir(5);
+		if(a[j] == ids[j]){
+			j++;
+		}else{
+			document.getElementById('resultado').innerHTML = "Perdeu";
+			setTimeout(function () {
+				document.location.reload(true);
+			}, 2000)
+		}
+		
+	};
 
 }
 
 var play = function (turnoD) {
 	disablePlay();
 
-	// // var j = 0;
-	// for (var i = cores.length - 1; i >= 0; i--) {
-	// 	(function (i) {
-	// 		setTimeout(function () {
-	// 			console.log("Teste" + i);
-	// 			blocos[i].classList.add(cores[i]);
-	// 			document.getElementById('resultado').innerHTML = i;
-	// 			if(i === 5){
-	// 				// document.getElementById('resultado').innerHTML = ;
-	// 			}
-
-	// 		}, 100*i);
-	// 	})(i);
-	// }
 	
 	// var turnoD = 3;
 	var a = [];
 	for (var i = 0; i < turnoD; i++) {
-		a[i] = ( Math.floor(Math.random() * 6));
+		a[i] = ( Math.floor(Math.random() * turnoD));
 	}
-	for (var i = a.length - 1; i >= 0; i--) {
+	for (var i = 0; i < 7; i++) {
 		(function (i) {
 			setTimeout(function () {
 				if(i != 0){
 					blocos[a[i-1]].classList.remove(cores[a[i-1]]);
 
 				}
-				console.log(a[i]);
-				blocos[a[i]].classList.add(cores[a[i]]);
-				document.getElementById('resultado').innerHTML = a[i] + 1;
-				if(i === 5){
-					// document.getElementById('resultado').innerHTML = ;
+				if (i < 6) {	
+					console.log(a[i]);
+					blocos[a[i]].classList.add(cores[a[i]]);
+					document.getElementById('resultado').innerHTML = a[i] + 1;
+				}else if(i == 6){
+					document.getElementById('resultado').innerHTML = "Teste sua memória";
+					payplay(a);
 				}
-
-			}, 300*i);
+			}, 500*i);
 		})(i);
 	}
-
-	console.log(a);
 }
